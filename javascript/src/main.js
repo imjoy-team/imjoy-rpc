edadfc7b3f8e17b61decf1d787e6d6235476307c
Wei Ouyang <oeway007@gmail.com>
2020-05-03 00:10:02 +0200

    Release to npm: v0.1.11 Move connection to imjoy-core (#2)
    * support ndarray
    
    * remove print
    
    * fix format
    
    * Remove connection
    
    * fix format
    
    * clean up export
    
    * Remove connection
    
    * clean up export
    
    * Bump version

--- javascript/src/main.js
+++ javascript/src/main.js
@@ -10,11 +10,10 @@
 import PluginWorker from "./plugin.webworker.js";
 import setupIframe from "./pluginIframe.js";
 import setupWebPython from "./pluginWebPython.js";
-import { setupServiceWorker, cacheRequirements, randId } from "./utils.js";
+import { setupServiceWorker, randId } from "./utils.js";
 
-export { RPC } from "./rpc.js";
-export { BasicConnection } from "./connection.js";
-export { Whenable, randId } from "./utils.js";
+export { RPC, API_VERSION } from "./rpc.js";
+export { version as VERSION } from "../package.json";
 
 function inIframe() {
   try {
--- javascript/src/utils.js
+++ javascript/src/utils.js
@@ -1,6 +1,3 @@
-import Ajv from "ajv";
-const ajv = new Ajv();
-
 export function randId() {
   return Math.random()
     .toString(36)
@@ -29,50 +26,6 @@ export const typedArrayToDtype = {
   Float64Array: "float64",
   Array: "array"
 };
-
-export const CONFIG_SCHEMA = ajv.compile({
-  properties: {
-    allow_execution: { type: "boolean" },
-    api_version: { type: "string" },
-    cover: { type: ["string", "array"] },
-    dedicated_thread: { type: "boolean" },
-    description: { type: "string", maxLength: 256 },
-    flags: { type: "array" },
-    icon: { type: "string" },
-    id: { type: "string" },
-    inputs: { type: ["object", "array"] },
-    labels: { type: "array" },
-    lang: { type: "string" },
-    name: { type: "string" },
-    outputs: { type: ["object", "array"] },
-    tags: { type: "array" },
-    token: { type: "string" },
-    ui: { type: "string" },
-    version: { type: "string" }
-  },
-  required: ["api_version", "allow_execution", "token", "id"]
-});
-
-export function compareVersions(v1, comparator, v2) {
-  comparator = comparator == "=" ? "==" : comparator;
-  if (
-    ["==", "===", "<", "<=", ">", ">=", "!=", "!=="].indexOf(comparator) == -1
-  ) {
-    throw new Error("Invalid comparator. " + comparator);
-  }
-  var v1parts = v1.split("."),
-    v2parts = v2.split(".");
-  var maxLen = Math.max(v1parts.length, v2parts.length);
-  var part1, part2;
-  var cmp = 0;
-  for (var i = 0; i < maxLen && !cmp; i++) {
-    part1 = parseInt(v1parts[i], 10) || 0;
-    part2 = parseInt(v2parts[i], 10) || 0;
-    if (part1 < part2) cmp = 1;
-    if (part1 > part2) cmp = -1;
-  }
-  return eval("0" + comparator + cmp);
-}
 
 function cacheUrlInServiceWorker(url) {
   return new Promise(function(resolve, reject) {
@@ -152,86 +105,6 @@ export function setupServiceWorker(targetOrigin, cacheCallback) {
     });
   }
 }
-
-/**
- * A special kind of event:
- *  - which can only be emitted once;
- *  - executes a set of subscribed handlers upon emission;
- *  - if a handler is subscribed after the event was emitted, it
- *    will be invoked immideately.
- *
- * Used for the events which only happen once (or do not happen at
- * all) during a single plugin lifecycle - connect, disconnect and
- * connection failure
- */
-export const Whenable = function(multi_emit) {
-  this._multi_emit = multi_emit;
-  this._emitted = false;
-  this._handlers = [];
-};
-
-/**
- * Emits the Whenable event, calls all the handlers already
- * subscribed, switches the object to the 'emitted' state (when
- * all future subscibed listeners will be immideately issued
- * instead of being stored)
- */
-Whenable.prototype.emit = function(e) {
-  if (this._multi_emit) {
-    this._emitted = true;
-    this._e = e;
-    for (let handler of this._handlers) {
-      setTimeout(handler.bind(null, e), 0);
-    }
-  } else if (!this._emitted) {
-    this._emitted = true;
-    this._e = e;
-    var handler;
-    while ((handler = this._handlers.pop())) {
-      setTimeout(handler.bind(null, e), 0);
-    }
-  }
-};
-
-/**
- * Saves the provided function as a handler for the Whenable
- * event. This handler will then be called upon the event emission
- * (if it has not been emitted yet), or will be scheduled for
- * immediate issue (if the event has already been emmitted before)
- *
- * @param {Function} handler to subscribe for the event
- */
-Whenable.prototype.whenEmitted = function(handler) {
-  handler = this._checkHandler(handler);
-  if (this._emitted) {
-    setTimeout(handler.bind(null, this._e), 0);
-  } else {
-    this._handlers.push(handler);
-  }
-};
-
-/**
- * Checks if the provided object is suitable for being subscribed
- * to the event (= is a function), throws an exception if not
- *
- * @param {Object} obj to check for being subscribable
- *
- * @throws {Exception} if object is not suitable for subscription
- *
- * @returns {Object} the provided object if yes
- */
-Whenable.prototype._checkHandler = function(handler) {
-  var type = typeof handler;
-  if (type !== "function") {
-    var msg =
-      "A function may only be subsribed to the event, " +
-      type +
-      " was provided instead";
-    throw new Error(msg);
-  }
-
-  return handler;
-};
 
 //#Source https://bit.ly/2neWfJ2
 export function urlJoin(...args) {
