07708eef7923f6ac1fa5b7e74d4211637f47bde6
Wei Ouyang <oeway007@gmail.com>
2020-05-09 01:16:07 +0200

    Fix debug console log

--- javascript/tests/main_test.js
+++ javascript/tests/main_test.js
@@ -65,7 +65,6 @@ describe("test", async () => {
     };
 
     coreConnection.on("initialized", data => {
-      console.log("=================>", data);
       const pluginConfig = data.config;
 
       if (!data.success) {
