62697dd66a4c98e289858e211cefd224f13972a5
Wei Ouyang <oeway007@gmail.com>
2020-06-03 02:07:09 +0200

    Make imjoy_jupyter_extension a python module

---
+++ jupyter/imjoy_jupyter_extension/__init__.py
@@ -0,0 +1,10 @@
+
+# Jupyter Extension points
+def _jupyter_nbextension_paths():
+    return [dict(
+        section="notebook",
+        src="static",
+        # directory in the `nbextension/` namespace
+        dest="imjoy_jupyter_extension",
+        # _also_ in the `nbextension/` namespace
+        require="imjoy_jupyter_extension/imjoy-rpc")]
