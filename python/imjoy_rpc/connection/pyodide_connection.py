65545f44b3c21aab8943cc709cfbfc1a2d57f97b
Wei Ouyang <oeway007@gmail.com>
2020-12-12 21:57:59 +0100

    Add  event loop for pyodide

--- python/imjoy_rpc/connection/pyodide_connection.py
+++ python/imjoy_rpc/connection/pyodide_connection.py
@@ -2,6 +2,8 @@ import uuid
 import sys
 import logging
 import re
+import heapq
+import asyncio
 
 from imjoy_rpc.rpc import RPC
 from imjoy_rpc.utils import MessageEmitter, dotdict
@@ -20,6 +22,95 @@ logger = logging.getLogger("Pyodide Connection")
 connection_id = contextvars.ContextVar("connection_id")
 
 
+class EventSimulator(asyncio.AbstractEventLoop):
+    """A simple event-driven simulator, using async/await
+    Author: @damonjw, see Gist: https://gist.github.com/damonjw/35aac361ca5d313ee9bf79e00261f4ea
+    """
+
+    def __init__(self):
+        self._time = 0
+        self._running = False
+        self._immediate = []
+        self._scheduled = []
+        self._exc = None
+
+    def get_debug(self):
+        return False
+
+    def time(self):
+        return self._time
+
+    def run_forever(self):
+        self._running = True
+        while (self._immediate or self._scheduled) and self._running:
+            if self._immediate:
+                h = self._immediate[0]
+                self._immediate = self._immediate[1:]
+            else:
+                h = heapq.heappop(self._scheduled)
+                self._time = h._when
+                h._scheduled = False  # just for asyncio.TimerHandle debugging?
+            if not h._cancelled:
+                h._run()
+            if self._exc is not None:
+                raise self._exc
+
+    def run_until_complete(self, future):
+        raise NotImplementedError
+
+    def _timer_handle_cancelled(self, handle):
+        pass
+
+    def is_running(self):
+        return self._running
+
+    def is_closed(self):
+        return not self._running
+
+    def stop(self):
+        self._running = False
+
+    def close(self):
+        self._running = False
+
+    def shutdown_asyncgens(self):
+        pass
+
+    def call_exception_handler(self, context):
+        self._exc = context.get("exception", None)
+
+    def call_soon(self, callback, *args, **kwargs):
+        h = asyncio.Handle(callback, args, self)
+        self._immediate.append(h)
+        return h
+
+    def call_later(self, delay, callback, *args):
+        if delay < 0:
+            raise Exception("Can't schedule in the past")
+        return self.call_at(self._time + delay, callback, *args)
+
+    def call_at(self, when, callback, *args):
+        if when < self._time:
+            raise Exception("Can't schedule in the past")
+        h = asyncio.TimerHandle(when, callback, args, self)
+        heapq.heappush(self._scheduled, h)
+        h._scheduled = True  # perhaps just for debugging in asyncio.TimerHandle?
+        return h
+
+    def create_task(self, coro):
+        async def wrapper():
+            try:
+                await coro
+            except Exception as e:
+                print("Wrapped exception")
+                self._exc = e
+
+        return asyncio.Task(wrapper(), loop=self)
+
+    def create_future(self):
+        return asyncio.Future(loop=self)
+
+
 class PyodideConnectionManager:
     def __init__(self, rpc_context):
         self.default_config = rpc_context.default_config
@@ -27,6 +118,11 @@ class PyodideConnectionManager:
         self.interface = None
         self.rpc_context = rpc_context
         self._codecs = {}
+
+        # Set the event loop for RPC
+        # This is needed because Pyodide does not support the default loop of asyncio
+        loop = EventSimulator()
+        asyncio.set_event_loop(loop)
 
     def get_ident(self):
         return connection_id.get(default=None)
