36408b62d21cbd7f4da488ce581c896dee4de4a3
Wei Ouyang <oeway007@gmail.com>
2020-12-13 16:04:57 +0100

    remove tests_require

--- python/setup.py
+++ python/setup.py
@@ -45,7 +45,6 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     install_requires=requirements,
-    tests_require=requirements + test_requirements,
     extras_require={
         "full": ["numpy", "zarr"],
         "jupyter": ["ipykernel", "numpy"],
