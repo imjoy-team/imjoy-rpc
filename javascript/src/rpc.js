1aee817e1ddbb151161123d9aa0c51788716f83a
Wei Ouyang <oeway007@gmail.com>
2020-12-13 19:52:00 +0100

    Fix format

--- javascript/src/rpc.js
+++ javascript/src/rpc.js
@@ -15,7 +15,6 @@ const ArrayBufferView = Object.getPrototypeOf(
   Object.getPrototypeOf(new Uint8Array())
 ).constructor;
 
-
 function _appendBuffer(buffer1, buffer2) {
   const tmp = new Uint8Array(buffer1.byteLength + buffer2.byteLength);
   tmp.set(new Uint8Array(buffer1), 0);
