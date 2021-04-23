65a2eab504ec6f10b48df43706ceed9a1dcfb1ce
Martin Hjelmare <marhje52@gmail.com>
2021-04-23 12:06:21 +0200

    Clean setup.py and requirements

--- python/setup.py
+++ python/setup.py
@@ -1,48 +1,40 @@
 """Set up the ImJoy-Engine imjoy package."""
 import json
-import os
-import sys
-from setuptools import setup, find_packages
+from pathlib import Path
 
+from setuptools import find_packages, setup
 
 DESCRIPTION = (
     "ImJoy Plugin Engine for running Python plugins locally "
     "or remotely from ImJoy.io"
 )
 
-ROOT_DIR = os.path.dirname(__file__)
+ROOT_DIR = Path(__file__).parent.resolve()
+README_FILE = ROOT_DIR / "README.md"
+LONG_DESCRIPTION = README_FILE.read_text(encoding="utf-8")
+VERSION_FILE = ROOT_DIR / "imjoy_rpc" / "VERSION"
+VERSION = json.loads(VERSION_FILE.read_text())["version"]
 
-
-def read(name):
-    """Read file name contents and return it."""
-    with open(os.path.join(ROOT_DIR, name)) as fil:
-        return fil.read()
-
-
-with open(os.path.join(ROOT_DIR, "imjoy_rpc", "VERSION"), "r") as f:
-    VERSION = json.load(f)["version"]
-
-if sys.version_info < (3, 5):
-    raise Exception("Python < 3.5 is not supported.")
-
-requirements = read("requirements.txt")
-requirements += '\ncontextvars;python_version<"3.7"'
-requirements += '\naiocontextvars;python_version<"3.7"'
-requirements += "\n"
+REQUIRES = [
+    "aiocontextvars; python_version<'3.7'",
+    "contextvars; python_version<'3.7'",
+    "werkzeug",
+]
 
 setup(
     name="imjoy-rpc",
     version=VERSION,
     description=DESCRIPTION,
-    long_description=read("README.md"),
+    long_description=LONG_DESCRIPTION,
     long_description_content_type="text/markdown",
     url="http://github.com/imjoy-team/imjoy-rpc",
     author="ImJoy Team",
     author_email="imjoy.team@gmail.com",
     license="MIT",
     packages=find_packages(),
+    python_requires=">=3.6",
     include_package_data=True,
-    install_requires=requirements,
+    install_requires=REQUIRES,
     extras_require={
         "full": ["numpy", "zarr"],
         "jupyter": ["numpy", "ipykernel"],
