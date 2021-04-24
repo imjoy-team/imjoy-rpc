b600e3909574d2f180342a1e3fac50eb4d76fb14
Martin Hjelmare <marhje52@gmail.com>
2021-04-24 11:27:16 +0200

    Add flake8 linting (#101)
    * Add flake8 linting
    
    * Lint init
    
    * Lint rpc
    
    * Lint utils
    
    * Lint serve wheel
    
    * Lint connection
    
    * Lint colab connection
    
    * Lint jupyter connection
    
    * Disable pyodide connection for now
    
    * Lint socketio connection
    
    * Run black
    
    * Comment out weird code

--- python/imjoy_rpc/connection/socketio_connection.py
+++ python/imjoy_rpc/connection/socketio_connection.py
@@ -1,3 +1,4 @@
+"""Provide a SocketIO connection."""
 import uuid
 import sys
 import asyncio
@@ -14,7 +15,10 @@ connection_id = contextvars.ContextVar("connection_id")
 
 
 class SocketIOManager:
+    """Represent a SocketIO manager."""
+
     def __init__(self, rpc_context):
+        """Set up instance."""
         self.default_config = rpc_context.default_config
         self.clients = {}
         self.interface = None
@@ -22,9 +26,11 @@ class SocketIOManager:
         self._codecs = {}
 
     def get_ident(self):
+        """Return identity."""
         return connection_id.get(default=None)
 
     def set_interface(self, interface, config=None):
+        """Set the interface."""
         config = config or self.default_config
         config = dotdict(config)
         config.name = config.name or "ImJoy Plugin"
@@ -39,6 +45,7 @@ class SocketIOManager:
             self.clients[k].rpc.set_interface(interface, self.default_config)
 
     def register_codec(self, config):
+        """Register codec."""
         assert "name" in config
         assert "encoder" in config or "decoder" in config
         if "type" in config:
@@ -51,6 +58,7 @@ class SocketIOManager:
         self._codecs[config["name"]] = dotdict(config)
 
     def init(self, config=None):
+        """Initialize the connection."""
         # register a minimal plugin api
         def setup():
             pass
@@ -58,31 +66,36 @@ class SocketIOManager:
         self.set_interface({"setup": setup}, config)
 
     def start(self, url):
+        """Start."""
         sio = socketio.AsyncClient()
 
         def registered(config):
+            """Handle registration."""
+
             @sio.on(config["channel"] + "-new-client")
             def on_new_client(client_info):
+                """Handle a new client."""
                 self._create_new_connection(
                     sio, config["channel"], client_info["channel"]
                 )
 
         @sio.event
         async def connect():
+            """Connect."""
             await sio.emit("register_plugin", {"id": sio.sid}, callback=registered)
 
         self.sio = sio
         asyncio.ensure_future(self.sio.connect(url))
 
     def _create_new_connection(self, sio, plugin_channel, client_channel):
-
+        """Create a new connection."""
         connection_id.set(client_channel)
         connection = SocketioConnection(
             self.default_config, sio, plugin_channel, client_channel
         )
 
         def initialize(data):
-
+            """Initialize connection."""
             config = self.default_config.copy()
             cfg = self.default_config
             if cfg.get("credential_required") is not None:
@@ -94,6 +107,7 @@ class SocketIOManager:
             rpc.init()
 
             def patch_api(_):
+                """Patch api."""
                 api = rpc.get_remote() or dotdict()
                 api.init = self.init
                 api.export = self.set_interface
@@ -116,7 +130,10 @@ class SocketIOManager:
 
 
 class SocketioConnection(MessageEmitter):
+    """Represent a SocketIO connection."""
+
     def __init__(self, config, sio, plugin_channel, client_channel):
+        """Set up instance."""
         self.config = dotdict(config or {})
         super().__init__(logger)
         self.sio = sio
@@ -126,6 +143,7 @@ class SocketioConnection(MessageEmitter):
 
         @sio.on(plugin_channel)
         def on_message(data):
+            """Handle a message."""
             if data.get("peer_id") == self.peer_id or data.get("type") == "initialize":
                 if "type" in data:
                     self._fire(data["type"], data)
@@ -138,17 +156,22 @@ class SocketioConnection(MessageEmitter):
 
         @sio.event
         def connect_error():
+            """Handle a connection error."""
             self._fire("connectFailure")
 
         @sio.event
         def disconnect():
+            """Handle disconnection."""
             self._fire("disconnected")
 
     def connect(self):
+        """Connect."""
         self._fire("connected")
 
     def disconnect(self):
+        """Disconnect."""
         pass
 
     def emit(self, msg):
+        """Emit a message."""
         asyncio.ensure_future(self.sio.emit(self.plugin_channel, msg))
